74dcfc701684ba4a325a0c7b28c634225c705c3f
nikhil <nickjr1032@gmail.com>
2024-07-24 23:56:00 +0530

    deleted swiper

--- componants/swiper/Swiper.jsx
+++ componants/swiper/Swiper.jsx
@@ -1,33 +1,33 @@
-"use client"
+// "use client"
 
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import React, { useRef, useState } from 'react';
+// // Import Swiper React components
+// import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
+// // Import Swiper styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
 
-import styles from './swiper.module.css'
-import prod from '../../../public/prod.jpg'
-import prod2 from '../../../public/prod2.jpg'
-import prod3 from '../../../public/prod3.jpg'
+// import styles from './swiper.module.css'
+// import prod from '../../../public/prod.jpg'
+// import prod2 from '../../../public/prod2.jpg'
+// import prod3 from '../../../public/prod3.jpg'
 
-// import required modules
-import { Navigation } from 'swiper/modules';
-import Image from 'next/image';
+// // import required modules
+// import { Navigation } from 'swiper/modules';
+// import Image from 'next/image';
 
-function Sliding() {
-  return (
-    <Swiper navigation={true} modules={[Navigation]} className={styles.swiper} >
+// function Sliding() {
+//   return (
+//     <Swiper navigation={true} modules={[Navigation]} className={styles.swiper} >
         
-        <SwiperSlide  className={styles.swiperSlide} >
-          <Image src={prod} className={styles.swiperImg} />
-          {/* SwiperSlide */}
-        </SwiperSlide>
+//         <SwiperSlide  className={styles.swiperSlide} >
+//           <Image src={prod} className={styles.swiperImg} />
+//           {/* SwiperSlide */}
+//         </SwiperSlide>
         
-      </Swiper>
-  )
-}
+//       </Swiper>
+//   )
+// }
 
-export default Sliding
+// export default Sliding
