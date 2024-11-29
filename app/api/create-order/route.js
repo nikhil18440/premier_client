194e9cddd05c042cf7c7b62040b07c6a9a8955cb
nikhil <nickjr1032@gmail.com>
2024-11-29 08:37:03 +0530

    made orderId local api change

--- app/api/create-order/route.js
+++ app/api/create-order/route.js
@@ -1,25 +1,25 @@
-import {NextRequest, NextResponse } from "next/server";
-import Razorpay from "razorpay";
+// import {NextRequest, NextResponse } from "next/server";
+// import Razorpay from "razorpay";
 
-const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET
-})
+// const razorpay = new Razorpay({
+//     key_id: process.env.RAZORPAY_KEY_ID,
+//     key_secret: process.env.RAZORPAY_KEY_SECRET
+// })
 
-export async function POST() {
-    try {
-        const order = await razorpay.orders.create({
-            amount: 100*100, //amount in paise,
-            currency: 'INR',
-            receipt: 'receipt_' + Math.random().toString(36).substring(7),
-        })
+// export async function POST() {
+//     try {
+//         const order = await razorpay.orders.create({
+//             amount: 100*100, //amount in paise,
+//             currency: 'INR',
+//             receipt: 'receipt_' + Math.random().toString(36).substring(7),
+//         })
 
-        return NextResponse.json({orderId: order.id}, {status:200})
-    } catch (error) {
-        console.log("Error creating order: ", error)
-        return NextResponse.json(
-            {error: 'Error creating order'},
-            {status: 500}
-        )
-    }
-}
+//         return NextResponse.json({orderId: order.id}, {status:200})
+//     } catch (error) {
+//         console.log("Error creating order: ", error)
+//         return NextResponse.json(
+//             {error: 'Error creating order'},
+//             {status: 500}
+//         )
+//     }
+// }
